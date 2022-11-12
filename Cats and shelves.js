5743e54aac29ca5289370b5aed5d3e579414da0c
Jalani Bray <112330628+JSSCodes@users.noreply.github.com>
2022-11-11 23:54:26 -0500

    Add files via upload

---
+++ Cats and shelves.js
@@ -0,0 +1,36 @@
+// Description
+// An infinite number of shelves are arranged one above the other in a staggered fashion.
+// The cat can jump up to 3 shelves at the same time: from shelf 1 to shelf 2 or 4 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+
+//                  ┌────────┐
+//                  │-6------│
+//                  └────────┘
+// ┌────────┐       
+// │------5-│        
+// └────────┘  ┌─────► OK!
+//             │    ┌────────┐
+//             │    │-4------│
+//             │    └────────┘
+// ┌────────┐  │
+// │------3-│  │     
+// BANG!────┘  ├─────► OK! 
+//   ▲  |\_/|  │    ┌────────┐
+//   │ ("^-^)  │    │-2------│
+//   │ )   (   │    └────────┘
+// ┌─┴─┴───┴┬──┘
+// │------1-│
+// └────────┘
+
+function solution(start, finish) 
+{
+  let stepsToClimb = finish-start
+
+  let numSmallJumps = stepsToClimb % 3
+  
+  
+  
+  let numBigJumps = Math.floor(stepsToClimb/3)
+
+
+  return numBigJumps + numSmallJumps
+}

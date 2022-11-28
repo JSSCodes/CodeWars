9fa0c0f07d1f066669683c913d957508d193c87b
Jalani Bray <112330628+JSSCodes@users.noreply.github.com>
2022-11-27 20:25:05 -0500

    Add files via upload

---
+++ Even or Odd.js
@@ -0,0 +1,9 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(numbers) {
+    if (numbers %2 == 0) {
+    return "Even";
+  } else{
+    return "Odd";
+  }
+};

d1d3afd47629254cbeedaf9d48eb207935312f73
Jalani Bray <112330628+JSSCodes@users.noreply.github.com>
2023-06-05 16:51:35 -0400

    Add files via upload

---
+++ esreveR.js
@@ -0,0 +1,7 @@
+// Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+// (the dedicated builtin(s) functionalities are deactivated)
+
+reverse = function(array){
+    return array.reduceRight(function(a, v){ return a.push(v), a }, []);
+  };

bbf4c24e1d7504947bbb0917d0c6bc2302129dc1
Jalani Bray <112330628+JSSCodes@users.noreply.github.com>
2023-09-06 08:14:18 -0400

    Add files via upload

---
+++ Changing letters.js
@@ -0,0 +1,13 @@
+// When provided with a String, capitalize all vowels
+
+// For example:
+
+// Input : "Hello World!"
+
+// Output : "HEllO WOrld!"
+
+// Note: Y is not a vowel in this kata.
+
+function swap(st){
+    return st.replace(/[aeiou]/g, v => v.toUpperCase() )
+  };

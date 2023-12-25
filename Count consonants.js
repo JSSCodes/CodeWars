b11042fbd76c2eb3edfbe87e58c82674ecdc58e5
Jalani Bray <112330628+JSSCodes@users.noreply.github.com>
2023-12-25 13:01:42 -0500

    Add files via upload

---
+++ Count consonants.js
@@ -0,0 +1,7 @@
+// Complete the function that takes a string of English-language text and returns the number of consonants in the string.
+
+// Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+
+function consonantCount(str){
+    return (str.match(/[b-df-hj-np-tv-z]/gi) || []).length;
+  };
